d69940603d2e50fe2bc932f85373dd95af7f76c0
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-02-05 10:07:52 -0500

    Update Customers.jsx

--- src/Customers/Customers.jsx
+++ src/Customers/Customers.jsx
@@ -4,6 +4,7 @@ import AddCustomer from "./AddCustomer";
 
 function Customers({customers}) {
     const [searchField, setSearchField] = useState([])
+    const [openAddCustomerModal, setOpenAddCustomerModal] = useState([])
     let filteredCustomers
      try {
     filteredCustomers = customers.map(customer => customer.data).data.filter((o) =>
