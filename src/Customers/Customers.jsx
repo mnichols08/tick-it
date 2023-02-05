e525e853049b1e2902f456785d4b87a770f29ae3
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-02-05 10:01:03 -0500

    Update Customers.jsx

--- src/Customers/Customers.jsx
+++ src/Customers/Customers.jsx
@@ -5,7 +5,30 @@ import Customer from "./Customer";
 import AddCustomer from "./AddCustomer";
 
 function Customers({customers}) {
+    let filteredCustomers
+     try {
+    filteredCustomers = customers.map(customer => customer.data).data.filter((o) =>
+      Object.keys(o).some((k) => {
 
+        if (o[k] !== null)
+          return o[k]
+            .toString()
+            .toLowerCase()
+            .includes(searchField.toString().toLowerCase());
+        })
+      );
+    } catch {
+     filteredCustomers = customers.filter(customer => 
+                                          customer.data.name.toString().toLowerCase().includes(searchField.toString().toLowerCase()) ||
+                                          customer.data.lookup && customer.data.lookup.toString().toLowerCase().includes(searchField.toString().toLowerCase()) ||
+                                          customer.data.phone && customer.data.phone.toString().toLowerCase().includes(searchField.toString().toLowerCase()) ||
+                                          customer.data.address && customer.data.address.toString().toLowerCase().includes(searchField.toString().toLowerCase())
+                                         )
+}
+filteredCustomers = filteredCustomers.sort((a, b) => (a.data.name > b.data.name) ? 1 : -1 );
+  const handleChange = (e) => {
+    setSearchField(e.target.value);
+  };
   return (
     <section>
       <div className="tickIt__container">
@@ -17,7 +40,7 @@ function Customers({customers}) {
         />
 
         <div className="tickIt__boxes">
-          {customers.map((customer) => (
+          {filteredCustomers.map((customer) => (
             <Customer
               name={customer.data.name}
               phone={customer.data.phone}
