b4a0ca28c61e707a101f046765ed23332e748632
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-02-05 12:35:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./tickIt.css";
 import Header from "./Header";
 import Footer from "./Footer";
-
-function App({ Element, customers }) {
+function App({ Element }) {
    
   return (
     <div className="app tickIt">
       <Header />
-      <Element customers={customers}/>
+      <Element />
       <Footer />
     </div>
   );
