c75299590868a88da3b750050624b1d6c23c90f2
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-02-05 12:24:10 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import { useEffect, useState } from 'react';
-import { collection, query, orderBy, onSnapshot, where, limit } from "firebase/firestore";
-import { db } from "./firebase";
 import "./tickIt.css";
 import Header from "./Header";
 import Footer from "./Footer";
 
-function App({ Element }) {
-    const [searchField, setSearchField] = useState([]);
-  const [openAddCustomerModal, setOpenAddCustomerModal] = useState(false);
-  const [customers, setCustomers] = useState([]);
-  /* function to get all customers from firestore in realtime */
-  useEffect(() => {
-    const customerColRef = query(
-      collection(db, "customers")
-    );
-    
-    onSnapshot(customerColRef, (snapshot) => {
-      setCustomers(
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          data: doc.data()
-        }))
-      );
-    });  
-  }, [ ]);
+function App({ Element, customers }) {
+   
   return (
     <div className="app tickIt">
       <Header />
