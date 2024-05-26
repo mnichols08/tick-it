551b18de41042718846f495599b38f9d2e70a869
Mikey Nichols <mikenichols410@gmail.com>
2024-05-26 08:42:50 -0400

    updates customers

--- src/Customers/Customers.jsx
+++ src/Customers/Customers.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { collection, query, orderBy, onSnapshot, where, limit } from "firebase/firestore";
+import { collection, query, startAfter, onSnapshot, where, limit } from "firebase/firestore";
 import { db } from "../firebase";
 import Customer from "./Customer";
 import AddCustomer from "./AddCustomer";
@@ -8,40 +8,58 @@ function Customers() {
   const [searchField, setSearchField] = useState([]);
   const [openAddCustomerModal, setOpenAddCustomerModal] = useState(false);
   const [customers, setCustomers] = useState([]);
-  const [filter, setFilter] = useState([]);
+  const [filteredCustomers, setFilteredCustomers] = useState([]);
   
-  let filteredCustomers
-     try {
-    filteredCustomers = customers.map(customer => customer.data).data.filter((o) =>
-      Object.keys(o).some((k) => {
+//   let filteredCustomers
+//      try {
+//     filteredCustomers = customers.map(customer => customer.data).data.filter((o) =>
+//       Object.keys(o).some((k) => {
 
-        if (o[k] !== null)
-          return o[k]
-            .toString()
-            .toLowerCase()
-            .includes(searchField.toString().toLowerCase());
-        })
-      );
-    } catch {
-     filteredCustomers = customers.filter(customer => 
-                                          customer.data.name.toString().toLowerCase().includes(searchField.toString().toLowerCase()) ||
-                                          customer.data.lookup && customer.data.lookup.toString().toLowerCase().includes(searchField.toString().toLowerCase()) ||
-                                          customer.data.phone && customer.data.phone.toString().toLowerCase().includes(searchField.toString().toLowerCase()) ||
-                                          customer.data.address && customer.data.address.toString().toLowerCase().includes(searchField.toString().toLowerCase())
-                                         )
-}
-filteredCustomers = filteredCustomers.sort((a, b) => (a.data.name > b.data.name) ? 1 : -1 );
+//         if (o[k] !== null)
+//           return o[k]
+//             .toString()
+//             .toLowerCase()
+//             .includes(searchField.toString().toLowerCase());
+//         })
+//       );
+//     } catch {
+//      filteredCustomers = customers.filter(customer => 
+//                                           customer.data.name.toString().toLowerCase().includes(searchField.toString().toLowerCase()) ||
+//                                           customer.data.lookup && customer.data.lookup.toString().toLowerCase().includes(searchField.toString().toLowerCase()) ||
+//                                           customer.data.phone && customer.data.phone.toString().toLowerCase().includes(searchField.toString().toLowerCase()) ||
+//                                           customer.data.address && customer.data.address.toString().toLowerCase().includes(searchField.toString().toLowerCase())
+//                                          )
+// }
+// filteredCustomers = filteredCustomers.sort((a, b) => (a.data.name > b.data.name) ? 1 : -1 );
   
   const handleChange = (e) => {
     setSearchField(e.target.value);
   };
   /* function to get all customers from firestore in realtime */
   useEffect(() => {
-    const customerColRef = query(
-      collection(db, "customers")
+    const customerNameColRef = query(
+      collection(db, "customers"),
+      where("name","==",searchField)
     );
-    
+    const customerAddressColRef = query(
+      collection(db, "customers"),
+      where("address","==",searchField)
+    );
+    const customerLookupColRef = query(
+      collection(db, "customers"),
+      where("lookup","==",searchField)
+    );
+    const customerPhoneColRef = query(
+      collection(db, "customers"),
+      where("phone","==",searchField)
+    );
+    const customerColRef = query(
+      collection(db, "customers"),
+      limit(20),
+      where("name","<=","9")
+    );
     onSnapshot(customerColRef, (snapshot) => {
+      console.log(snapshot.docs.map(doc => doc.data()))
       setCustomers(
         snapshot.docs.map((doc) => ({
           id: doc.id,
@@ -49,7 +67,7 @@ filteredCustomers = filteredCustomers.sort((a, b) => (a.data.name > b.data.name)
         }))
       );
     });  
-  }, [ ]);
+  }, [ searchField ]);
   return (
     <section>
     {console.log(customers)}
@@ -62,7 +80,7 @@ filteredCustomers = filteredCustomers.sort((a, b) => (a.data.name > b.data.name)
         />
 
         <div className="tickIt__boxes">
-          {filteredCustomers.map((customer) => (
+          {customers.map((customer) => (
             <Customer
               name={customer.data.name}
               phone={customer.data.phone}
